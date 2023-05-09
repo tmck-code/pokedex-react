2231161f3c6554ee161f095cd2166026c5309f12
Tom McKeesick <tmck01@gmail.com>
2023-05-09 20:08:41 +1000

    Add initial POC models for Cards & Sets

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .db import Base
+
+
+class Set(Base):
+    __tablename__ = "sets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(String)
+
+    items = relationship("Image", back_populates="owner")
+
+
+class Card(Base):
+    __tablename__ = "cards"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    image_url = Column(String, index=False)
+    set_id = Column(Integer, ForeignKey("sets.id"))
+
+    owner = relationship("Set", back_populates="cards")

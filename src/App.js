baa11a7cc4004f442b02892b22e16b492fa696ab
Tom McKeesick <tmck01@gmail.com>
2023-05-12 20:35:59 +1000

    Reduce to a more minimal working api call

--- src/App.js
+++ src/App.js
@@ -1,92 +1,35 @@
-import {useState, useCallback} from 'react';
+import {useState, useCallback, useEffect} from 'react';
 import './App.css';
 
-function MyButton() {
-  const [data, setData] = useState({data: []});
-  const [isLoading, setIsLoading] = useState(false);
-  const [err, setErr] = useState('');
-
-  const sendRequest = useCallback(async () => {
-    setIsLoading(true);
-
-    try {
-      const response = await fetch('http://localhost:8000/cards', {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-        },
-	mode:'cors'
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
-      }
-
-      const result = await response.json();
-
-      console.log('result is: ', JSON.stringify(result, null, 4));
-
-      setData(result);
-    } catch (err) {
-      setErr(err.message);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [isLoading]) // update the callback if the state changes
-
-  return (
-    <input type="button" onClick={sendRequest} />
-  )
-
-}
-
-const cards = [
-  {
-    name: 'Erika\'s Hospitality',
-    imageUrl: 'https://den-cards.pokellector.com/288/Erikas-Hospitality.SM12A.190.30530.png',
-    imageSize: 90,
-  }
-];
-
 function Cards() {
-  const listItems = cards.map(card =>
-    <li
-      key={card.name}
-      style={{
-        color: true ? 'magenta' : 'darkgreen'
-      }}
-    >
-      <i>{card.name}</i>
-      <img
-        className="avatar"
-        src={card.imageUrl}
-        alt={'Photo of ' + card.name}
-        style={{
-          width: card.imageSize,
-          height: card.imageSize
-        }}
-      />
+  const [data, setData] = useState([])
+
+  const fetchData = async ()=>{
+    let res = await fetch('http://localhost:8000/cards/')
+    let data = await res.json()
+    setData(data)
+  }
+
+  useEffect(() => {
+      fetchData()
+  }, [])
+
+  const listCards = data.map((card) =>
+    <li key={card.number_in_set}>
+      <p>{JSON.stringify(card)}</p>
     </li>
   );
 
   return (
-    <ul>{listItems}</ul>
+    <ul>{listCards}</ul>
   );
 }
-
-const handleClick = async () => {
-}
-
-
-
-
 
 export default function MyApp() {
   return (
     <div>
       <Cards />
       <h1>Welcome to my app</h1>
-      <MyButton />
     </div>
   );
 }

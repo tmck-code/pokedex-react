6f38a3f67d1d35548321eef8bbe2d036948a8088
Tom McKeesick <tmck01@gmail.com>
2023-05-12 21:43:08 +1000

    working grid!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import {useState, useEffect} from 'react';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+
 import './App.css';
+
 
 function Cards() {
   const [data, setData] = useState([])
@@ -16,16 +19,21 @@ function Cards() {
   }, [])
 
   // <img src={`data:image/jpeg;base64,${card.image_url}`} alt="secret"/>
+  //  <li key={card.number_in_set}>
+  //    <p>{card.title}</p>
+  //    <p>{JSON.stringify(card)}</p>
+  //  </li>
   const listCards = data.map((card) =>
-    <li key={card.number_in_set}>
-      <p>{card.title}</p>
-      <p>{JSON.stringify(card)}</p>
+    <Grid>
       <img className="avatar" src={require('./' + card.image_url)} alt="logo" />
-    </li>
+      <p>{card.name}</p>
+    </Grid>
   );
 
   return (
-    <ul>{listCards}</ul>
+    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+      {listCards}
+    </Grid>
   );
 }
 

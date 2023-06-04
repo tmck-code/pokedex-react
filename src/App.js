522b9977a3fa16d577494faa4c45fe735fe33af8
Tom McKeesick <tmck01@gmail.com>
2023-06-04 19:19:31 +1000

    hide/show image working

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+// for the zoom
+import Switch from '@mui/material/Switch';
+import Paper from '@mui/material/Paper';
+import Zoom from '@mui/material/Zoom';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { Theme } from '@mui/material/styles';
 
 
 import './App.css';
@@ -13,6 +19,7 @@ import './App.css';
 
 function Cards() {
   const [data, setData] = useState([])
+  const [checked, setChecked] = React.useState(false);
 
   const fetchData = async ()=>{
     let res = await fetch('http://localhost:8000/cards/')
@@ -25,31 +32,40 @@ function Cards() {
       fetchData()
   }, [])
 
-  // <img src={`data:image/jpeg;base64,${card.image_url}`} alt="secret"/>
-  //  <li key={card.number_in_set}>
-  //    <p>{card.title}</p>
-  //    <p>{JSON.stringify(card)}</p>
-  //  </li>
-  // <img className="avatar" src={require('./' + card.image_url)} alt="logo" />
-  // <!-- the max width of any pokemon image is 592 -->
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+
   const listCards = data.map((card) =>
-    <Grid>
-      <Box>
-        <Card variant="outlined" sx={{ maxWidth: 300, height: 650 }}>
-          <CardMedia
-            component="img"
-            image={require('./' + card.image_url)}
-            alt={card.title}
+  <Grid>
+
+      <Box sx={{ height: 180 }}>
+        <FormControlLabel
+          control={<Switch checked={checked} onChange={handleChange} />}
+          label="Show"
           />
-          <CardContent>
-            <Typography variant="body1" color="text.secondary" component="div">
-	      #{card.number_in_set}
-            </Typography>
-            <Typography variant="h4" component="div">
-              {card.title}
-            </Typography>
-          </CardContent>
-        </Card>
+        <Box sx={{ display: 'flex' }}>
+          <Zoom in={checked}>
+            <Box>
+              {/* <!-- the max width of any pokemon image is 592 --> */}
+              <Card variant="outlined" sx={{ maxWidth: 300, height: 650 }}>
+                <CardMedia
+                  component="img"
+                  image={require('./' + card.image_url)}
+                  alt={card.title}
+                />
+                <CardContent>
+                  <Typography variant="body1" color="text.secondary" component="div">
+                    #{card.number_in_set}
+                  </Typography>
+                  <Typography variant="h4" component="div">
+                    {card.title}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Box>  
+          </Zoom>
+        </Box>
       </Box>
     </Grid>
   );
@@ -60,6 +76,7 @@ function Cards() {
     </Grid>
   );
 }
+
 
 export default function MyApp() {
   return (

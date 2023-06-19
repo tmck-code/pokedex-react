388188e5b4d643b65d5f9bf31cb6b9c354e69a99
Tom McKeesick <tmck01@gmail.com>
2023-06-19 20:58:20 +1000

    add a menu?

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+
+import Divider from '@mui/material/Divider';
+import Paper from '@mui/material/Paper';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ContentCut from '@mui/icons-material/ContentCut';
+import ContentCopy from '@mui/icons-material/ContentCopy';
+import ContentPaste from '@mui/icons-material/ContentPaste';
+import Cloud from '@mui/icons-material/Cloud';
 // for the zoom
 // import Switch from '@mui/material/Switch';
 // import Paper from '@mui/material/Paper';
@@ -107,9 +118,38 @@ function Cards() {
 }
 
 
+function IconMenu() {
+  return (
+    <Paper sx={{ width: 320, maxWidth: '100%' }}>
+      <MenuList>
+        <MenuItem>
+          <ListItemIcon>
+            <ContentCut fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Sort By Name</ListItemText>
+          <Typography variant="body2" color="text.secondary">
+            ⌘X
+          </Typography>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <ContentCopy fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Sort By Number</ListItemText>
+          <Typography variant="body2" color="text.secondary">
+            ⌘C
+          </Typography>
+        </MenuItem>
+      </MenuList>
+    </Paper>
+  );
+}
+
+
 export default function MyApp() {
   return (
     <div>
+      <IconMenu />
       <Cards />
     </div>
   );

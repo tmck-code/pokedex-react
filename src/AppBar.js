590eb01197d769062e5cff32e768e0ec08c31928
Tom McKeesick <tmck01@gmail.com>
2023-07-01 11:12:45 +1000

    separate main bits out into files
    turns out that importing stuff in react is pretty easy! The toolbar code
    gets long quickly so it's not feasible to continue putting everything in
    1 file

---
+++ src/AppBar.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import { useState, useEffect } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import Modal from '@mui/material/Modal';
+import SearchIcon from '@mui/icons-material/Search';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
+  
+  export default function SearchAppBar() {
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar component="nav">
+          <Toolbar>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, textAlign: 'right' }}
+            >
+              tmck-code/pokedex-react
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  }
+  
---
+++ src/CardGrid.js
@@ -0,0 +1,95 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+
+const style = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  "&:hover": {
+    backgroundcolor: "red"
+  }
+};
+  
+// <Button onClick={(e) => handleClick(card.image_url)}>#{card.number_in_set}</Button>
+function CardModal(image, open, handleClose) {
+  return (
+    <Modal
+      sx={style}
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Card variant="outlined">
+          <CardMedia component="img" image={require('./' + image)} alt="pokemon" />
+        </Card>
+      </Box>
+    </Modal>
+  );
+};
+  
+export default function Cards() {
+  const [data, setData] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [image, setCard] = useState(true);
+
+  const handleClick = (value) => {
+    setCard(value);
+    handleOpen();
+    console.log(value, image, open);
+  };
+
+
+  const fetchData = async ()=>{
+    let res = await fetch('http://localhost:8000/cards/')
+    let data = await res.json()
+    console.log('response', data)
+    setData(data)
+  }
+
+  useEffect(() => {
+      fetchData()
+  }, [])
+
+  const listCards = data.map((card) => (
+    <Grid key={"grid-"+card.number_in_set+"-"+card.title}>
+      <ButtonBase
+        key={"button-"+card.number_in_set+"-"+card.title}
+        onClick={event => handleClick(card.image_url)}
+      >
+      <Box>
+        <Card variant="outlined" sx={{ maxWidth: 300, maxHeight: 650 }}>
+          <CardMedia
+            component="img"
+            image={require('./' + card.image_url)}
+            alt={card.title}
+            />
+          <CardContent>
+            <Typography variant="h5" component="div">#{card.number_in_set}: {card.title}</Typography>
+          </CardContent>
+        </Card>
+      </Box>
+      </ButtonBase>
+    </Grid>
+  ));
+  
+  return (
+    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+      {listCards}
+      {open && CardModal(image, open, handleClose)}
+    </Grid>
+  );
+}
